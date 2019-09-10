b9b9e6e0eae59da6f13da7f82052fd128d25a055
一拳超人 <602401890@qq.com>
2019-09-10 16:06:03 +0800

    后台项目

--- demo-I18n/src/components/vuexStudy.vue
+++ demo-I18n/src/components/vuexStudy.vue
@@ -2,6 +2,27 @@
     <div>
         <h1>{{this.$store.state.count}}</h1>
 
-        
+        <input type="text">+
+        <input type="text">=
+        <input type="text">
     </div>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    component:{
+
+    }
+}
+</script>
+<style lang="">
+    input{
+        width: 100px;
+        height: 50px;
+        background-color: yellow;
+    }
+</style>
--- demo-I18n/src/router/index.js
+++ demo-I18n/src/router/index.js
@@ -3,13 +3,18 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
-
+const vuexStudy = resolve => require(['@/components/vuexStudy.vue'], resolve)
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: 'vuexStudy',
+      name: '测试',
+      component: vuexStudy
     }
   ]
 })
